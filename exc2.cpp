a40d18d9470ccc1cf13dd9b1dbc3ed8d5e82d1ba
Vergacas <jeanvergacas@gmail.com>
2024-03-11 12:07:57 -0300

    atualizada questão

--- exc2.cpp
+++ exc2.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <cmath>
 
+/**
+ * função para conferir se a entrada, o valor recebido, faz parte da sequencia de Fibonnaci.
+ * A entrada da função é um inteiro.
+ * A saida é um booleano:
+ *      True: se a entrada faz parte da sequencia de Fibonnaci.
+ *      False: se a entrada não faz parte da sequencia de Fibonnaci.
+ * 
+*/
 bool fibonnaci(int entrada) {
     int fib_ant = 1;
     int fib = 1;
@@ -20,9 +28,14 @@ bool fibonnaci(int entrada) {
     
 }
 
+/**
+ * Classe main, classe pricipal do pragrama.
+ * Soliciata um inteiro como entrada e imprime se a entrada faz parte da sequecia de Fibonnaci ou não;
+*/
 int main(int argc, char const *argv[])
 {
     int en = 0;
+    std::cout << "Número para teste: ";
     std::cin >> en;
     if (fibonnaci(en))
     {
